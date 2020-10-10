fa99dd75cad98e21c7d030ba0e288bb17eb866e0
yenilikci <melihcelik017@outlook.com>
2020-10-10 17:48:53 +0300

    Butonlara işlemler eklendi, Head ve Footer ayrı eklendi, Basit hesap makinesi uygulaması tamamlandı

--- App.js
+++ App.js
@@ -1,23 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import Header from './src/component/header';
+import Footer from './src/component/footer';  
 
 export default class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      input1: ''
+      input1: '',
+      input2: '',
+      result: 0
     }
+
+    this.sum = this.sum.bind(this);
+    this.deduct = this.deduct.bind(this);
+    this.mult = this.mult.bind(this);
+    this.div = this.div.bind(this);
+
+  }
+
+  sum(){
+    const number1 = parseFloat(this.state.input1);
+    const number2 = parseFloat(this.state.input2);
+    const result = number1 + number2;
+    this.setState({
+      result: result
+    })
+  }
+
+  deduct(){
+    const number1 = parseFloat(this.state.input1);
+    const number2 = parseFloat(this.state.input2);
+    const result = number1 - number2;
+    this.setState({
+      result: result
+    })
+  }
+  
+  mult(){
+    const number1 = parseFloat(this.state.input1);
+    const number2 = parseFloat(this.state.input2);
+    const result = number1 * number2;
+    this.setState({
+      result: result
+    })
+  }
+
+  div(){
+    const number1 = parseFloat(this.state.input1);
+    const number2 = parseFloat(this.state.input2);
+    const result = number1 / number2;
+    this.setState({
+      result: result
+    })
   }
 
   render(){
   return (
     <View style={styles.container}>  
 
-      <View style={styles.header}>
-          <Text style={styles.headerText}>Hesap Makinesi</Text>
-      </View>
+      <Header headerText="Hesap Makinesi"/>
 
       <View style={styles.contentWrapper}>
         <TextInput 
@@ -30,7 +74,55 @@ export default class App extends Component {
             }}
             value = {this.state.input1}
         />
-        <Text style={styles.sampleText}>İlk Sayı: {this.state.input1}</Text>
+
+        <View style={styles.buttonWrapper}>
+
+            <View style={styles.button}>
+            <Button onPress={this.sum}
+              color="#8aaa79"
+              title= "+"
+            />
+            </View>
+
+            <View style={styles.button}>
+            <Button onPress={this.deduct}
+              color="#183059"
+              title= "-"
+            />
+            </View>
+
+            <View style={styles.button}>
+            <Button onPress={this.mult}
+              color="#e08e45"
+              title= "*"
+            />
+            </View>
+
+            <View style={styles.button}>
+            <Button onPress={this.div}
+              color="#841584"
+              title= "/"
+            />
+            </View>
+
+
+        </View>
+
+        <TextInput 
+        style={styles.input}
+            placeholder = 'İkinci Sayı'
+            onChangeText = {(text) => {
+               this.setState({
+                 input2: text
+               })
+            }}
+            value = {this.state.input2}
+        />
+
+        <Text style={styles.sampleText}>Sonuç: {this.state.result}</Text>
+
+        <Footer footerText="Muhammed Melih Çelik"/>
+
       </View>
 
       <StatusBar style="auto" />
@@ -50,25 +142,25 @@ const styles = StyleSheet.create({
     flexDirection: 'column',
     justifyContent: 'center',
   },
-  input1: {
+  buttonWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  button:{
+    width: 50,
+    height: 50,
+    margin: 5,
+  },
+  input: {
     height: 40,
+    fontSize: 20,
+    marginBottom: 15,
+    borderBottomColor: '#000',
+    borderBottomWidth: 2
   },  
   sampleText: {
     height: 30,
-    fontSize: 14,
+    fontSize: 24,
+    marginTop: 10,
   },
-  header: {
-    height:90,
-    paddingTop:30,
-    shadowColor: '#000000',
-    shadowOffset: {width:3 ,height:5}, 
-    shadowOpacity: 0.7,
-    backgroundColor: '#f5fcff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  headerText: {
-    fontSize:20,
-    textAlign: 'center'
-  }
 });
